5e6f465e22d2b1c84e68965b0cb47d539079e8f5
duwestyin <yin_yanjie@outlook.com>
2019-11-04 16:42:53 +0800

    CART算法 只做了分类树 还没有剪枝 没有考虑缺失属性 没有做回归树

--- task02/CART_predict_C.m
+++ task02/CART_predict_C.m
@@ -1,9 +1,27 @@
 function result = CART_predict_C(tree, sample)
- M = length(sample);
  while(1)
+     %叶子节点的情况
      if ~isempty(tree.class)
-         result = tree.clss;
+         result = tree.class;
          return;
      end
+     %离散的情况
+     if tree.split_left ~= tree.split_right
+          if sample(tree.attribute) == tree.split_left
+             tree = tree.child_left;
+          else
+             tree = tree.child_right;
+          end
+     else
+         %连续的情况
+         if sample(tree.attribute) <= tree.split_left
+             tree = tree.child_left;
+         else
+             tree = tree.child_right;
+         end
+         
+     end 
  end
+ 
+ 
 end
